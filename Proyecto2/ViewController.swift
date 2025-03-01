2576ad151b8f9357d6b2e3deadc65210f935a564
buneasassin <bune_assassin@hotmail.com>
2025-02-28 22:23:44 -0600

    Initial Commit

---
+++ Proyecto2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto2
+//
+//  Created by Dante Raziel Basurto Saucedo on 28/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
