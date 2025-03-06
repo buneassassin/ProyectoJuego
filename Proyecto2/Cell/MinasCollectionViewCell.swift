1d1bbe4cbf686ab0e807d5eae6a39ef521157496
“TorresTG” <totilotegabriel@gmail.com>
2025-03-06 11:00:33 -0600

    toma dante

---
+++ Proyecto2/Cell/MinasCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MinasCollectionViewCell.swift
+//  Proyecto2
+//
+//  Created by Tobias Rodriguez Lujan on 06/03/25.
+//
+
+import UIKit
+
+class MinasCollectionViewCell: UICollectionViewCell {
+
+   @IBOutlet weak var imgEstado: UIImageView!
+   
+   override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
