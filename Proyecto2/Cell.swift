e606790b63230bfc8824b8605fbe9e2148920c1c
buneasassin <bune_assassin@hotmail.com>
2025-03-06 09:48:05 -0600

    mas

---
+++ Proyecto2/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  Proyecto2
+//
+//  Created by Dante Raziel Basurto Saucedo on 06/03/25.
+//
+
+import Foundation
+// Cell.swift
+struct Cell {
+    var isRevealed: Bool = false
+    var hasMine: Bool = false
+    var adjacentMines: Int = 0
+}
